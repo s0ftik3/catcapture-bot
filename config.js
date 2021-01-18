68d8e0640e27a941ab27c15007b9cf15f5861204
s0ftik3 <slava021323@gmail.com>
2021-01-18 19:19:38 +0600

    1.5.1

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     bot: {
         "name": "Cat Capture",
         "bot": "@catcapturebot",
-        "version": "1.5.0", 
+        "version": "1.5.1", 
         "token": process.env.TOKEN,
         "database": process.env.DATABASE, 
         "admin": 511695340, // Telegram ID.
